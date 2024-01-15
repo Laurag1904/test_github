efff0102731198beca848482b34a998a75c2b7bd
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-01-15 10:31:20 +0100

    made a change

---
+++ src/laura/Marina.java
@@ -0,0 +1,10 @@
+package laura;
+
+public class Marina {
+
+	public static void main(String[] args) {
+		
+		System.out.println("local commit test");
+	}
+
+}
---
+++ src/laura/test.java
@@ -0,0 +1,10 @@
+package laura;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Commit Message Test");
+	}
+
+}
