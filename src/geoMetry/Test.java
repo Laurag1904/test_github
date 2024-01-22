821130f6d4a4d69bcc76022c3f639f99a44d6d2f
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-01-22 11:04:03 +0100

    Create Test Coordinates

--- src/geoMetry/Test.java
+++ src/geoMetry/Test.java
@@ -1,5 +1,10 @@
 package geoMetry;
 
 public class Test {
+	
+	Coordinate c1 = new Coordinate(5, 10);
+	Coordinate c2 = new Coordinate(327, 3485);
+	Coordinate c3 = new Coordinate(27346, 20384);
+	
 
 }
