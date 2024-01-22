8f5f724fb860b4324a8bd7307e1a6ecc9cf36652
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-01-22 10:28:22 +0100

    constructor

--- src/geoMetry/Triangle.java
+++ src/geoMetry/Triangle.java
@@ -1,5 +1,21 @@
 package geoMetry;
 
-public class Triangle {
+public class Triangle extends Shapes {
+	
+	Coordinate point1;
+	Coordinate point2;
+	Coordinate point3;
+    double[] sideLengths;
+
+    public Triangle(Coordinate point1, Coordinate point2, Coordinate point3) {
+        this.point1 = point1;
+        this.point2 = point2;
+        this.point3 = point3;
+        this.sideLengths = new double[] {
+            calculateSide(point1, point2),
+            calculateSide(point2, point3),
+            calculateSide(point3, point1)
+        };
+    }
 
 }
