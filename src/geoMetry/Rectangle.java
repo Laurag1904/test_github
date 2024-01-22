60be6d8a13186db5d4ea39f2a20f65a72085c7bd
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-01-22 10:36:46 +0100

    create attributes with constructor

--- src/geoMetry/Rectangle.java
+++ src/geoMetry/Rectangle.java
@@ -1,22 +1,16 @@
 package geoMetry;
 
-public class Rectangle {
+public class Rectangle extends Shapes{
 	
-	private double length;
-	private double width;
-	
-	public double getLength() {
-		return length;
-	}
-	public void setLength(double length) {
-		this.length = length;
-	}
-	public double getWidth() {
-		return width;
-	}
-	public void setWidth(double width) {
-		this.width = width;
-	}
+	Coordinate point1;
+    Coordinate point2;
+ 
+
+    public Rectangle(Coordinate point1, Coordinate point2) {
+    	super();
+        this.point1 = point1;
+        this.point2 = point2;
+    }
 	
 	
 	
