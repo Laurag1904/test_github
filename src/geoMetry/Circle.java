b97d3016e2992772542281e3cdd938148c73661c
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-01-22 10:13:07 +0100

    getter and setter

--- src/geoMetry/Circle.java
+++ src/geoMetry/Circle.java
@@ -11,5 +11,30 @@ public class Circle extends Shapes{
 		this.middle = middle;
 		this.radiusPoint = radiusPoint;
 	}
+	
+	public Coordinate getMiddle() {
+		return middle;
+	}
+
+	public void setMiddle(Coordinate middle) {
+		this.middle = middle;
+	}
+
+	public Coordinate getRadiusPoint() {
+		return radiusPoint;
+	}
+
+	public void setRadiusPoint(Coordinate radiusPoint) {
+		this.radiusPoint = radiusPoint;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
 
 }
