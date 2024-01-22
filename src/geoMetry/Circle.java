14bc0172f7155d8035b189aacb24512f50797d72
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-01-22 10:12:04 +0100

    define constructor

--- src/geoMetry/Circle.java
+++ src/geoMetry/Circle.java
@@ -5,5 +5,11 @@ public class Circle extends Shapes{
 	Coordinate middle;
 	Coordinate radiusPoint;
 	double radius;
+	
+	public Circle(Coordinate middle, Coordinate radiusPoint) {
+		super();
+		this.middle = middle;
+		this.radiusPoint = radiusPoint;
+	}
 
 }
