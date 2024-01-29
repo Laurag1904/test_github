73e878646fc2026625ffcb103ef859a0af6961ce
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-01-29 11:15:37 +0100

    Methode to calculate Total Area

--- src/geoMetry/Group.java
+++ src/geoMetry/Group.java
@@ -1,8 +1,39 @@
 package geoMetry;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Group {
 	
-	public double addArea() {
-		return 0;
+	private class Group {
+	    private ArrayList<Shape> shapes = new ArrayList<>();
+
+	    public void addShape(Shape shape) {
+	        shapes.add(shape);
+	    }
+
+	    public void removeShape(Shape shape) {
+	        shapes.remove(shape);
+	    }
+
+	    public double totalArea() {
+	        double totalArea = 0;
+	        Set<Shape> seenShapes = new HashSet<>();
+
+	
+	    }
 	}
+	
+	public double calculateTotalArea() {
+        double totalArea = 0;
+        Set<Shapes> seenShapes = new HashSet<>();
+        for (Shapes shape : shapess) {
+            if (!seenShapes.contains(shape)) {
+                totalArea += shape.calculateArea();
+                seenShapes.add(shape);
+            }
+        }
+        return totalArea;
+    }
+
 }
