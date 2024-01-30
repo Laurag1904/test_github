28fba97ca8f6447878d25b72a693d8f9c940578e
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-01-30 09:22:39 +0100

    Created attributes

--- src/socialNetwork/NewsFeed.java
+++ src/socialNetwork/NewsFeed.java
@@ -1,5 +1,11 @@
 package socialNetwork;
 
+import java.util.ArrayList;
+
 public class NewsFeed {
 
+	 private ArrayList<NewsFeed> newsfeed;
+	 private String title;
+	 private String content;
+	 
 }
