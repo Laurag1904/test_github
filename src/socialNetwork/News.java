10f01f4f2a2c7d2b1ab3e41406172d695a815803
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-02-05 11:10:46 +0100

    Attributes and getter and setter

--- src/socialNetwork/News.java
+++ src/socialNetwork/News.java
@@ -1,5 +1,46 @@
 package socialNetwork;
 
+import java.time.LocalDate;
+
 public class News {
+	
+	private String name;
+	private String text;
+	private LocalDate Date;
+	private String readers;
+	private String comment;
+	
+		public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public LocalDate getDate() {
+		return Date;
+	}
+	public void setDate(LocalDate date) {
+		Date = date;
+	}
+	public String getReaders() {
+		return readers;
+	}
+	public void setReaders(String readers) {
+		this.readers = readers;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
 
 }
