b371339df904ebf3b5d987ef416ee06f443f1d6f
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-02-05 11:16:01 +0100

    attributes + getter and setter

--- src/socialNetwork/Photo.java
+++ src/socialNetwork/Photo.java
@@ -1,5 +1,54 @@
 package socialNetwork;
 
+import java.time.LocalDate;
+
 public class Photo {
+	
+	private String name;
+	private String dataname;
+	private String foto;
+	private LocalDate Time;
+	private String like;
+	private String comment;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDataname() {
+		return dataname;
+	}
+	public void setDataname(String dataname) {
+		this.dataname = dataname;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public LocalDate getTime() {
+		return Time;
+	}
+	public void setTime(LocalDate time) {
+		Time = time;
+	}
+	public String getLike() {
+		return like;
+	}
+	public void setLike(String like) {
+		this.like = like;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
 
 }
