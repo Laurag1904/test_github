f5fc6d48e791c7894e18900cfb6ebb1b03f4d599
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-02-27 09:43:56 +0100

    Not complete

--- src/socialNetwork/ForYou.java
+++ src/socialNetwork/ForYou.java
@@ -1,5 +1,31 @@
 package socialNetwork;
 
-public class ForYou {
+import java.util.ArrayList;
 
+public class ForYou {
+	
+	private ArrayList<NewsFeed> NewsFeed;
+	
+	public void Texteinsendungen () {
+		for( Object NewsFeed : NewsFeed) {
+			if(ForYou instanceof NewsFeed) {
+				System.out.println("");
+				
+			}else if (ForYou instanceof News) {
+				
+			}
+		}
+		
+	}
+
+	
+	public void Fotoeinsendungen () {
+		for () {
+			if() {
+				System.out.println();
+			}else if () {
+				
+			}
+		}
+	}
 }
