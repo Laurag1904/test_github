1ebe87283cda9b8d6e40dd421d5002c57788a758
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-03-04 11:44:40 +0100

    Methode Kommentare filtern

---
+++ src/array/Kommentarefiltern.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kommentarefiltern {
+	
+	 //List<String> lines = List.of("Eintrag 1", "Eintrag 2", "Eintrag 3", "Eintrag 4");
+	
+	 
+	public static void reduceToComments(List<String> lines) {
+		ArrayList<String>list= new ArrayList<String>();
+		for(int i = 0; i<=4 ; i= i+3) {
+			if (lines.size ()< 4) {
+				System.out.println("Liste hat nicht genug Eintrage");	
+			}
+			
+		}
+		for(int i = 0; i< ArrayList<String>list; i = i+3) {
+			
+		}
+		
+	}
+
+}
