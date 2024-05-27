3bf03ddb45dfdb63961a750e1f7868e9565d90ad
Laurag1904 <laura.grubelnik@hak-reutte.ac.at>
2024-05-27 11:40:43 +0200

    27.05 erweiterung der aufgabe

--- src/exceptions/MusikNotenLeser.java
+++ src/exceptions/MusikNotenLeser.java
@@ -1,24 +1,41 @@
 package exceptions;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MusikNotenLeser {
 
 	public static void main(String[] args) {
+		List<String> noten = Arrays.asList("C", "D", "E", "F", "G", "A", "B","C,", "D,", "E,", "F,", "G,", "A,", "B,", "c", "d", "e", "f", "g", "a","b", "c'", "d'", "e'", "f'", "g'", "a'","b'");
 		Path filePath = Paths.get("file.txt");
+		Path outputFilePath = Paths.get("output.txt");
 
-        try (Scanner scanner = new Scanner(filePath)) {
+        try (Scanner scanner = new Scanner(filePath);
+        	PrintWriter writer = new PrintWriter(Files.newBufferedWriter(outputFilePath))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                System.out.println(line);
+                if(noten.contains(line)) {
+                	System.out.println(line);
+                }
+                
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 	}
+	
+	
+	 private static boolean isValidLine(String line) {
+	        return line.matches("[CDEFGABcdefgab', ]+");
+	 }
+	 
+	 
 
 }
